92216e4416999ca0b1b830bd32d2491c57e2c6fc
David Miheev <me@davidmiheev.life>
2024-10-16 20:24:47 +0300

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -19,20 +19,20 @@ students = pd.DataFrame(students_data)
 
 # Generate sample data for tasks
 tasks_data = {
-    'task_id': [f'TK{str(i).zfill(3)}' for i in range(1, 111)],  # 110 tasks
-    'task_name': [f'Task {i}' for i in range(1, 111)],
-    'module_name': [f'Module {random.randint(1, 2)}' for _ in range(110)],
-    'course_name': [f'Course {random.randint(1, 3)}' for _ in range(110)]
+    'task_id': [f'TK{str(i).zfill(3)}' for i in range(1, 151)],  # 150 tasks
+    'task_name': [f'Task {i}' for i in range(1, 151)],
+    'module_name': [f'Module {random.randint(1, 2)}' for _ in range(150)],
+    'course_name': [f'Course {random.randint(1, 3)}' for _ in range(150)]
 }
 
 tasks = pd.DataFrame(tasks_data)
 
 # Generate sample data for attempts
 attempts_data = {
-    'user_id': [random.choice(students['user_id']) for _ in range(80)],
-    'task_id': [random.choice(tasks['task_id']) for _ in range(80)],
-    'attempt_number': [random.randint(1, 5) for _ in range(80)],
-    'attempt_status': [random.choice(['success', 'fail', 'error']) for _ in range(80)]
+    'user_id': [random.choice(students['user_id']) for _ in range(1200)],
+    'task_id': [random.choice(tasks['task_id']) for _ in range(1200)],
+    'attempt_number': [random.choice([1] * 10 + [2] * 20 + [3] * 10 + [4, 5, 6, 7, 8, 9, 10]) for _ in range(1200)],
+    'attempt_status': [random.choice(['success'] * 30 + ['fail', 'error']) for _ in range(1200)]
 }
 
 attempts = pd.DataFrame(attempts_data)
@@ -42,13 +42,16 @@ def adjust_attepmts():
     for _, row in attempts.iterrows():
         user, task, att_num, att_status = row['user_id'], row['task_id'], row['attempt_number'], row['attempt_status']
         for i in range(1, att_num):
-            new_row_df = pd.DataFrame([{'user_id': user, 'task_id': task, 'attempt_number': i, 'attempt_status': random.choice(['success', 'fail', 'error'])}])
+            if att_status != 'success':
+                new_row_df = pd.DataFrame([{'user_id': user, 'task_id': task, 'attempt_number': i, 'attempt_status': random.choice(['fail', 'error'])}])
+            else:
+                new_row_df = pd.DataFrame([{'user_id': user, 'task_id': task, 'attempt_number': i, 'attempt_status': random.choice(['success'] * 2 + ['fail', 'error'])}])
             adjusted_attempts = pd.concat([adjusted_attempts, new_row_df], ignore_index=True)
 
         new_row_df = pd.DataFrame([{'user_id': user, 'task_id': task, 'attempt_number': att_num, 'attempt_status': att_status}])
         adjusted_attempts = pd.concat([adjusted_attempts, new_row_df], ignore_index=True)
 
-    print(len(adjusted_attempts))
+    print(len(adjusted_attempts)) # total number of attempts
     return adjusted_attempts
 
 attempts = adjust_attepmts()
@@ -59,7 +62,7 @@ print(students)
 students.to_csv('students.csv', columns={'user_id', 'first_name', 'last_name', 'user_email'}, index=False)
 print("\nTasks DataFrame:")
 print(tasks)
-tasks.to_csv('tasks.csv',  columns={'task_id', 'task_name', 'module_name', 'course_name'}, index=False)
+tasks.to_csv('tasks.csv', columns={'task_id', 'task_name', 'module_name', 'course_name'}, index=False)
 print("\nAttempts DataFrame:")
 print(attempts)
 attempts.to_csv('attempts.csv', columns={'user_id', 'task_id', 'attempt_number', 'attempt_status'}, index=False)
@@ -70,7 +73,7 @@ attempts.to_csv('attempts.csv', columns={'user_id', 'task_id', 'attempt_number',
 
 query_1 =  '''
 with user_task_attempts as (
-select s.user_id, task_id, array_agg(attempt_status) as attempt_statuses
+select s.user_id as user_id, task_id, array_agg(attempt_status) as attempt_statuses
 from students as s
 left join attempts as a
 on s.user_id = a.user_id
@@ -89,9 +92,14 @@ print(result)
 result.to_csv('attention_to_this_students.csv', columns = {'student', 'hard_tasks_for_student'}, index=False)
 
 query_2 = '''
-with task_complexity as (
-select task_id, min(if(attempt_status = 'success', attempt_number, 100)) as complexity
+with task_complexity_per_user as (
+select task_id, user_id, min(if(attempt_status = 'success', attempt_number, 10)) as complexity_for_user
 from attempts as a
+group by task_id, user_id
+),
+task_complexity as (
+select task_id, avg(complexity_for_user) as complexity
+from task_complexity_per_user
 group by task_id
 ),
 ranked_tasks as (
@@ -99,7 +107,7 @@ select
   module_name || '_' || course_name as course_uid,
   t.task_id,
   complexity,
-  ROW_NUMBER() OVER (PARTITION BY module_name || '_' || course_name ORDER BY complexity DESC) AS rank,
+  DENSE_RANK() OVER (PARTITION BY module_name || '_' || course_name ORDER BY complexity DESC) AS rank,
   COUNT(*) OVER (PARTITION BY module_name || '_' || course_name) AS total_count
 FROM tasks as t
 left join task_complexity as tc
@@ -121,3 +129,94 @@ ORDER BY
 result = duckdb.query(query_2).to_df()
 print(result)
 result.to_csv('very_complex_tasks.csv', columns = {'course_uid', 'task', 'rank', 'complexity'}, index=False)
+
+query_3 = '''
+with task_complexity_per_user as (
+select task_id, user_id, min(if(attempt_status = 'success', attempt_number, 10)) as complexity_for_user
+from attempts as a
+group by task_id, user_id
+),
+task_complexity as (
+select task_id, avg(complexity_for_user) as complexity
+from task_complexity_per_user
+group by task_id
+),
+ranked_tasks as (
+select
+  module_name || '_' || course_name as course_uid,
+  t.task_id,
+  complexity,
+  DENSE_RANK() OVER (PARTITION BY module_name || '_' || course_name ORDER BY complexity ASC) AS rank,
+  COUNT(*) OVER (PARTITION BY module_name || '_' || course_name) AS total_count
+FROM tasks as t
+left join task_complexity as tc
+on t.task_id = tc.task_id
+)
+select
+    course_uid,
+    task_id as task,
+    rank,
+    complexity
+FROM
+  ranked_tasks
+WHERE
+    rank <= total_count * 0.05
+ORDER BY
+    course_uid, rank;
+'''
+
+result = duckdb.query(query_3).to_df()
+print(result)
+result.to_csv('very_easy_tasks.csv', columns = {'course_uid', 'task', 'rank', 'complexity'}, index=False)
+
+query_4 = '''
+with a as (
+select
+    s.user_id as user_id,
+    first_name,
+    last_name,
+    user_email,
+    task_id,
+    attempt_number,
+    attempt_status
+from students as s
+left join attempts as a
+on s.user_id = a.user_id
+)
+select
+    user_id as user_id,
+    first_name,
+    last_name,
+    user_email,
+    t.task_id as task_id,
+    task_name,
+    module_name,
+    course_name,
+    attempt_number,
+    attempt_status
+from a
+right join tasks as t
+on a.task_id = t.task_id
+'''
+result = duckdb.query(query_4).to_df()
+print(result)
+result.to_csv('joined_data.csv', columns = {'user_id', 'first_name', 'last_name', 'user_email', 'task_id', 'task_name', 'module_name', 'course_name', 'attempt_number', 'attempt_status'}, index=False)
+
+
+query_5 = '''
+with task_complexity_per_user as (
+select task_id, s.user_id as user_id, first_name || ' ' || last_name as user_name, min(if(attempt_status = 'success', attempt_number, 10)) as complexity_for_user
+from attempts as a
+right join students as s
+on a.user_id = s.user_id
+group by task_id, s.user_id, user_name
+)
+select user_name, module_name, course_name, task_name, complexity_for_user
+from task_complexity_per_user as tc
+right join tasks as t
+on tc.task_id = t.task_id
+'''
+
+result = duckdb.query(query_5).to_df()
+print(result)
+result.to_csv('student_performance.csv', columns = {'user_name', 'module_name', 'course_name', 'task_name', 'complexity_for_user'}, index=False)
